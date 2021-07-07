d75432c1af81bf5ecbea7ecb3527ce255414b2e3
Shibumishra <sibumishra487@gmail.com>
2021-07-07 17:59:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Pages/Home/Index';
+import Quiz from './Pages/Quiz';
+import Result from './Pages/Result';
+import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async( category = '', difficulty ='' )=>{
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    )
+    setQuestions(data.results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app" style={{backgroundImage: "url(./ques1.png)"}}>
+      <Header />
+      <Switch>
+        <Route path='/' exact >
+           <Home name={name} setName={setName} fetchQuestions={fetchQuestions} /> 
+        </Route>
+        <Route path="/quiz" >
+          <Quiz name={name} 
+          questions={questions} 
+          score={score} 
+          setScore={setScore} 
+          setQuestions={setQuestions} 
+          />
+        </Route>
+        <Route path="/result">
+          <Result name={name} score={score} />
+        </Route> 
+      </Switch>
     </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+               @Quiz
+            <p>Contact us : 99557793905</p> 
+            <p>Email - sibumishra546@gmail.com</p>
+
+        </div>
+    )
+}
+
+export default Footer
